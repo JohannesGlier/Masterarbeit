e6c5e0a520604ac1d72e863404fa9daf3ac6d98b
JohannesGlier <joo.glier@web.de>
2025-04-07 10:26:49 +0200

    Hovertexte für Toolbar-Buttons

--- src/components/Canvas/CanvasToolbar/CanvasToolbar.jsx
+++ src/components/Canvas/CanvasToolbar/CanvasToolbar.jsx
@@ -16,42 +16,49 @@ const CanvasToolbar = () => {
     return (
       <div className={styles['canvasToolbar']}>
         <button
+          title="Pointer-Tool zum Auswählen, Verschieben und Anpassen von Elementen"
           className={`${styles['button']} ${selectedTool === 'Pointer' ? styles.selectedButton : ''}`} 
           onClick={() => setSelectedTool('Pointer')}
         >
           <BiSolidPointer />
         </button>
         <button
+          title="Textkarten-Tool zum Erstellen von Textkarten"
           className={`${styles['button']} ${selectedTool === 'TextCard' ? styles.selectedButton : ''}`} 
           onClick={() => setSelectedTool('TextCard')}
         >
           <RiTextBlock />
         </button>
         <button
+          title="Bereich-Tool zum Erstellen von Bereichen"
           className={`${styles['button']} ${selectedTool === 'Frame' ? styles.selectedButton : ''}`} 
           onClick={() => setSelectedTool('Frame')}
         >
           <MdOutlineRectangle />
         </button>
         <button
+          title="Verbindungs-Tool zum Erstellen von Verbindungen"
           className={`${styles['button']} ${selectedTool === 'Arrow' ? styles.selectedButton : ''}`} 
           onClick={() => setSelectedTool('Arrow')}
         >
           <CgArrowLongUp style={{ transform: 'rotate(45deg)' }} />
         </button>
         <button
+          title="Schere-Tool zum automatischen Aufteilen von Textkarten"
           className={`${styles['button']} ${selectedTool === 'Scissor' ? styles.selectedButton : ''}`} 
           onClick={() => setSelectedTool('Scissor')}
         >
           <FiScissors />
         </button>
         <button
+          title="Intelligentes Textkarten-Erstellungs-Tool zum Erstellen von Textkarten mit automatisch generiertem Inhalt"
           className={`${styles['button']} ${selectedTool === 'AITextcard' ? styles.selectedButton : ''}`} 
           onClick={() => setSelectedTool('AITextcard')}
         >
           <LuSquareMousePointer />
         </button>
         <button
+          title="Auto-Layout-Tool zum automatischen Anordnen von Elementen auf dem Canvas"
           className={`${styles['button']} ${selectedTool === 'AutoLayout' ? styles.selectedButton : ''}`} 
           onClick={() => setSelectedTool('AutoLayout')}
         >
